b3f7346289917ec8a394f7aee1d608718683e4f9
Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-01-16 13:36:14 +0500

    3.3-lsn count of digits

---
+++ NumberOfDigits/NumberOfDigits/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace NumberOfDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\ACer\Downloads\Telegram Desktop\testTXT.txt";
+            var digits = GetNumberOfDigits(filePath);
+            foreach (var digit in digits)
+            {
+                Console.WriteLine(digit);
+            }
+
+        }
+
+        public static List<int> GetNumberOfDigits(string filePath)
+        {
+            List<int> ints = new List<int>();
+            using (StreamReader streamReader = new(filePath))
+            {
+                string? line = string.Empty;
+                while (true)
+                {
+                    line = streamReader.ReadLine();
+                    if (line == null) break;
+                    int count = line.Count(character => char.IsDigit(character));
+                    ints.Add(count);
+                }
+
+            }
+            return ints;
+        }
+    }
+}
